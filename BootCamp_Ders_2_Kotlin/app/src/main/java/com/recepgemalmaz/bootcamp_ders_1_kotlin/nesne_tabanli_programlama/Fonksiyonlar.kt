1af4d5386fb486dee82ed191e189b48621ebf766
Recep Gemalmaz <76572594+recepgemalmaz@users.noreply.github.com>
2023-09-04 22:24:17 +0300

    BootCamp_Ders_2_Kotlin
    BootCamp_Ders_2_Kotlin

---
+++ BootCamp_Ders_2_Kotlin/app/src/main/java/com/recepgemalmaz/bootcamp_ders_1_kotlin/nesne_tabanli_programlama/Fonksiyonlar.kt
@@ -0,0 +1,8 @@
+package com.recepgemalmaz.bootcamp_ders_1_kotlin.nesne_tabanli_programlama
+
+class Fonksiyonlar {
+
+    //void : geri donus degeri olmayan fonksiyonlar
+    //return : geri donus degeri olan fonksiyonlar
+    //parametre : disardan deger alabilen fonksiyonlar
+}
